ac61387e7045eb720d0ab701307e6d59d60a3781
Juan Nava <juan.ns17@gmail.com>
2020-07-31 03:36:41 +0000

    used promise function to replace our call back structure in the ISS app

--- index.js
+++ index.js
@@ -2,34 +2,6 @@
 // index.js
 const { nextISSTimesForMyLocation } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('-------')
-//   console.log('It worked! Returned IP:' , ip.ip);
-// });
-
-// fetchCoordsByIP('70.48.36.66', (error, coords) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('-------')
-//   console.log('It worked! Returned Coords:' , coords);
-
-// });
-
-// fetchISSFlyOverTimes({ latitude: '43.85010', longitude: '-79.51630' }, (error, schedule) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('-------')
-//   console.log('It worked! Returned ISS FlyOver Schedule:' , schedule);
-// });
-
 const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
     const datetime = new Date(0);
@@ -47,4 +19,3 @@ nextISSTimesForMyLocation((error, passTimes) => {
   printPassTimes(passTimes);
 });
 
-//nextISSTimesForMyLocation((msg) => console.log(msg));
